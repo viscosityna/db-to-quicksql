25b8120611803a977030a838346c68c453090845
Emanuel Cifuentes <emanuelcif@gmail.com>
2018-04-17 09:49:23 -0600

    Added a couple of loops, one for the tables and one for the columns.

--- QS_PARSE_METADATA.sql
+++ QS_PARSE_METADATA.sql
@@ -1,20 +1,23 @@
-create or replace package QS_PARSE_METADATA as
-	TYPE QS_COL_REC IS RECORD (
+CREATE OR REPLACE PACKAGE QS_PARSE_METADATA AS
+	TYPE T_QS_COL IS RECORD (
 		COLUMN_NAME VARCHAR2(100),
 		COLUMN_TYPE	VARCHAR2(50),
 		COLUMN_DIRECTIVES VARCHAR2(100)
 	);
 
-	TYPE QS_COLS_TAB IS TABLE OF QS_COL_REC;
+	TYPE T_QS_COLS IS TABLE OF T_QS_COL;
 
-	TYPE QS_TABLE IS RECORD (
+	TYPE T_QS_TABLE IS RECORD (
 		TABLE_NAME VARCHAR2(100),
 		TABLE_DIRECTIVES VARCHAR2(100),
-		TABLE_COLS QS_COLS_TAB
+		TABLE_COLS T_QS_COLS
 	);
 
-	--IDENTATION
+	TYPE T_QS_TABLES IS TABLE OF T_QS_TABLE;
+
+	--TEXT FORMATTING
 	G_IDNT CHAR(2) := '  ';
+	G_LN_BRK CHAR(1) := CHR(10);
 	--QUICK SQL COLUMN TYPES
 	G_T_NUM 	VARCHAR2(5) := 'num';
 	G_T_INT 	VARCHAR2(5) := 'int';
@@ -27,17 +30,34 @@ create or replace package QS_PARSE_METADATA as
 	G_T_FILE 	VARCHAR2(5) := 'file';
 
 	--QUICK SQL TABLE DIRECTIVES
-	G_D_COMPRESSED VARCHAR2(10) := ' /compress';
+	G_D_COMPRESSED VARCHAR2(10) := ' /compress'; --Table will be created compressed.
 
-	--QUICK SQL 
+	--QUICK SQL COLUMN DIRECTIVES
+	G_D_IDX	VARCHAR2(20) := ' /idx'; --	Will create a non unique index on the associated column.
+	G_D_UNQ VARCHAR2(20) := ' /unique'; --Creates a unique constraint.
+	G_D_CHK VARCHAR2(20) := ' /check'; --Creates a check constraint with with comma or white space delimited values e.g. /cc Y N
+	G_D_NN 	VARCHAR2(20) := ' /nn'; --Adds a not null constraint on the column.
+	G_D_BTN VARCHAR2(20) := ' /between'; --Adds a between check constraint on the column.
+	G_D_FK 	VARCHAR2(20) := ' /fk'; --Foreign key references e.g. /references table_name.
+	G_D_PK 	VARCHAR2(20) := ' /pk'; --	Identifies single column as table primary key.
 
+	PROCEDURE READ_TABLES ( T_QS_TABLES IN OUT NOCOPY T_QS_TABLES);
 
-	G_NUM VARCHAR2(10) := 'num';
-	G_NUM VARCHAR2(10) := 'num';
-	G_NUM VARCHAR2(10) := 'num';
-	G_NUM VARCHAR2(10) := 'num';
-	G_NUM VARCHAR2(10) := 'num';
-	G_NUM VARCHAR2(10) := 'num';
-	G_NUM VARCHAR2(10) := 'num';
+end QS_PARSE_METADATA;
 
-end QS_PARSE_METADATA; //
+CREATE OR REPLACE PACKAGE BODY QS_PARSE_METADATA AS
+	PROCEDURE READ_TABLES ( T_QS_TABLES IN OUT NOCOPY T_QS_TABLES)
+	IS
+	BEGIN
+		FOR TBLS IN (
+			SELECT 'TB1' AS TABL_NAME, 'C1' AS COLUMN_NAME, 'NUM' as COLUMN_TYPE
+			FROM DUAL
+			UNION ALL
+			SELECT 'TB1' AS TABL_NAME, 'C2' AS COLUMN_NAME, 'VARCHAR2' as COLUMN_TYPE
+			FROM DUAL
+			)
+		LOOP
+			
+		END LOOP; 
+	END;
+END QS_PARSE_METADATA; 
